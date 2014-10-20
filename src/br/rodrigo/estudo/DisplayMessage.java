b4370819b8ef555cfda4b3bfb59e4055474d6181
Rodrigocs10 <rodrigocs10@gmail.com>
2014-10-19 22:57:31 -0300

    Salvando estado da aplicação Tentativa 1

--- src/br/rodrigo/estudo/DisplayMessage.java
+++ src/br/rodrigo/estudo/DisplayMessage.java
@@ -15,14 +15,39 @@ public class DisplayMessage extends MainActivity {
 		String text = intent.getStringExtra(MainActivity.MENSAGEM);
 		
 		TextView textView = new TextView(this);
-		textView.setText(text);
-		
+		textView.setText(text + ", current level: " + getmCurrentLevel() + 
+				"Currente score: " + getmCurrentScore());
+				
+		// Check whether we're recreating a previously destroyed instance
+				if (savedInstanceState != null) {
+					// Restore value of members from saved state
+					setmCurrentLevel(savedInstanceState.getInt(STATE_LEVEL));
+					setmCurrentScore(savedInstanceState.getInt(STATE_SCORE));
+					 
+				} else {
+					// Probably initialize members with default values for a new
+					// instance
+				}
+				
 		setContentView(textView);
 		
 		// Quando a Acitivity que criamos extende Activity(necessário para criar thema customziado) 
 		// ao invés de ActionBarActivity não podemos usar getSupportActionBar()
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		
+	}
+	
+	public void onRestart(){
+		super.onRestart();
+		setmCurrentLevel(getmCurrentLevel() + 1);
+		setmCurrentScore(getmCurrentScore() + 1);
+	}
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		savedInstanceState.putInt(STATE_SCORE, 1);
+		savedInstanceState.putInt(STATE_LEVEL, 2);
+
+		// Always call the superclass so it can save the view hierarchy state
+		super.onSaveInstanceState(savedInstanceState);
 	}
 
 }
--- src/br/rodrigo/estudo/MainActivity.java
+++ src/br/rodrigo/estudo/MainActivity.java
@@ -11,11 +11,24 @@ import android.widget.EditText;
 public class MainActivity extends ActionBarActivity {
 
 	public static final String MENSAGEM = "br.rodrigo.estudo.Mensagem";
+
+	// Valores usados para testar salvamento de status via onSaveInstanceState
+	static final String STATE_SCORE = "playerScore";
+	static final String STATE_LEVEL = "playerLevel";
+
+	private int mCurrentScore;
+	private int mCurrentLevel;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mCurrentLevel++;
+		mCurrentScore++;
+		
+		
+
 	}
 
 	@Override
@@ -31,27 +44,46 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		switch(id){
-			case R.id.action_buscar:{
-				Intent intent = new Intent(this, ActivityBuscar.class);
-				startActivity(intent);
-			}
-			default:{
-				return super.onOptionsItemSelected(item);
-			}
+		switch (id) {
+		case R.id.action_buscar: {
+			Intent intent = new Intent(this, ActivityBuscar.class);
+			startActivity(intent);
 		}
-		
+		default: {
+			return super.onOptionsItemSelected(item);
+		}
+		}
+
 	}
-	
-	public void sendMessage(View view){
+
+	public void sendMessage(View view) {
 		EditText editText = (EditText) findViewById(R.id.edit_text);
 		String texto = editText.getText().toString();
-		
+
 		Intent intent = new Intent(this, DisplayMessage.class);
 		intent.putExtra(MENSAGEM, texto);
-		
+
 		startActivity(intent);
-		
+
 	}
+
 	
+
+	
+	protected int getmCurrentScore() {
+		return mCurrentScore;
+	}
+
+	protected void setmCurrentScore(int mCurrentScore) {
+		this.mCurrentScore = mCurrentScore;
+	}
+
+	protected int getmCurrentLevel() {
+		return mCurrentLevel;
+	}
+
+	protected void setmCurrentLevel(int mCurrentLevel) {
+		this.mCurrentLevel = mCurrentLevel;
+	}
+
 }
